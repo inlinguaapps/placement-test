9c7104befb259a88af4c76ca1e2feec05ec123c4
inlinguaapps <inlinguaapps@gmail.com>
2026-04-18 12:39:34 +0700

    adds shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <h1>inlingua Placement Test</h1>
+      <h1 className='text-xl my-4'>inlingua Placement Test</h1>
+      <Button>Doesn&apos;t do anything yet</Button>
     </div>
   )
 }
